00f973ab4f4a20f5a326141ecb419ec00ac8d23b
alfonsoc2 <alfonsocenteno2@gmail.com>
2024-11-14 16:55:02 -0700

    added new game function that resets the board

--- ttc.js
+++ ttc.js
@@ -1,9 +1,18 @@
 const gridCells = document.querySelectorAll('.gameGrid');
 
+
 gridCells.forEach((cell, index) => {
     cell.addEventListener('click', () => {
         if (!cell.textContent) {
             cell.textContent = 'X';
         }
+    });
+});
+
+const reset = document.getElementById('reset');
+
+reset.addEventListener('click', () => {
+    gridCells.forEach((cell) => {
+        cell.textContent ='';
     });
 });
